74e8cf9f556016ca2de01f168bd3dff0a8195936
Allen Ahern <ahernag@hotmail.com>
2019-04-04 16:53:16 -0500

    Clean code changes to allow multiple tokens for a given calcultion type symbol. Part 3 (noob move).

---
+++ Pluralsight/CalcEngine/src/org/ahernistan/pluralsight/calcengine/OpSymbolAndTokensGroups.java
@@ -0,0 +1,13 @@
+package org.ahernistan.pluralsight.calcengine;
+
+import java.util.Iterator;
+
+public class OpSymbolAndTokensGroups {
+
+    OperationSymbolAndTokens[] symbolAndTokensGroup;
+
+    OpSymbolAndTokensGroups(OperationSymbolAndTokens... symbolAndTokensGroup) {
+        this.symbolAndTokensGroup = symbolAndTokensGroup;
+    }
+
+}
